c7797af905b3a88d3650c9bc630d75dee6b97552
carloeusebi <carloeusebi@gmail.com>
2023-07-19 16:42:45 +0200

    Added validation inside the function

--- functions.php
+++ functions.php
@@ -1,13 +1,33 @@
 <?php
 
-function generate_password(int $pwd_length = 8, bool $allow_repetition = true): string
-{
-  $RANDOM_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_@[]`#!"$%&/()=';
 
-  $chars = str_split($RANDOM_CHARS);
+/**
+ * @return password returns NULL if there are not enough characters to generate a password based on the characters requirements
+ */
+function generate_password(int $pwd_length = 8, bool $allow_repetition = true, array $allowed_chars = []): string|NULL
+{
+  $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $numbers = '0123456789';
+  $symbols = '-_@[]`#!"$%&/()=';
+
+  $chars = '';
+
+  // mounts the chars strings
+  if (empty($allowed_chars)) {
+    $chars = $letters . $numbers . $symbols;
+  } else {
+    foreach ($allowed_chars as $allowed_char) {
+      $chars .= $$allowed_char;
+    }
+  }
+
+  $chars = str_split($chars);
+
+  // if repetition is not allowed, and there are not enough characters returns false
+  if (!$allow_repetition && $pwd_length > count($chars))
+    return NULL;
 
   $password = [];
-
   do {
     $char_index = rand(0, count($chars) - 1);
 
